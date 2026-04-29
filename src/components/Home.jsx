9659c9ffcbed182a903cd18ea27fa177feac1222
WallaceMichael <wallacemelo147@gmail.com>
2026-04-29 09:15:43 -0300

    first commit

---
+++ src/components/Home.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <>
+      <header className="shadow" > 
+        
+            <div className='bg-dark text-center py-3'>
+                <span className='text-white'>Your fashion store</span>
+            </div>
+            
+
+      </header>
+    </>
+  )
+}
+
+export default Home

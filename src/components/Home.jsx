828d93b08ba2f9e054912e2e6e525a2c16822a70
WallaceMichael <wallacemelo147@gmail.com>
2026-05-04 13:16:31 -0300

    🦄 refactor:  styles and enhance Home component layout with Bootstrap integration

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,18 +1,49 @@
-import React from 'react'
+import React from 'react';
+import { Nav, Navbar } from "react-bootstrap";
+import logo from '../assets/images/logo.png'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faUser } from '@fortawesome/free-regular-svg-icons'
+import { faCartShopping } from '@fortawesome/free-solid-svg-icons'
+
+
 
 const Home = () => {
   return (
     <>
-      <header className="shadow" > 
-        
-            <div className='bg-dark text-center py-3'>
-                <span className='text-white'>Your fashion store</span>
+      <header className="shadow">
+        <div className="bg-dark text-center py-3">
+          <span className="text-white">Your fashion store</span>
             </div>
             
+        <div className="container">
+          <Navbar expand="lg" className="bg-body-tertiary">
+              <Navbar.Brand href="#"><img src={logo} alt="" width={170} /></Navbar.Brand>
+              <Navbar.Toggle aria-controls="navbarScroll" />
+              <Navbar.Collapse id="navbarScroll">
+                <Nav
+                  className="ms-auto my-2 my-lg-0"
+                  style={{ maxHeight: "100px" }}
+                  navbarScroll
+                >
+                  <Nav.Link href="#action1">Mens</Nav.Link>
+                  <Nav.Link href="#action2">Womens</Nav.Link>
+                  <Nav.Link href="#action3">Kids</Nav.Link>
+                </Nav>
+                <div className="nav-right d-flex">
+                  <a href="">
+                    <FontAwesomeIcon icon={faUser} />
+                  </a>
 
+                  <a href="" className="ms-3">
+                    <FontAwesomeIcon icon={faCartShopping} />
+                  </a>
+                </div>
+              </Navbar.Collapse>
+          </Navbar>
+        </div>
       </header>
     </>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
